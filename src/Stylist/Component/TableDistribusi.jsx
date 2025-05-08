4aea08051dfe76c84601693510f7347e62b0fde8
Carlo Alghaniy <themortalsinner@gmail.com>
2025-05-08 14:13:38 +0700

    menambahkan add stok dan add sekolah pada page distribusi

--- src/Stylist/Component/TableDistribusi.jsx
+++ src/Stylist/Component/TableDistribusi.jsx
@@ -116,20 +116,18 @@ const TableDistribusi = () => {
                                     {item.statusBarang}
                                 </span>
                             </td>
-
-
-                            {/* Action Button */}
                             <td className="p-3 text-center">
+
+                                {/* Detail Distribusi */}
                                 <Link to="/DetailDis">
                                     <button className="px-4 py-2 bg-yellow-500 text-white rounded hover:bg-yellow-600 transition-colors">
                                         Detail
                                     </button>
                                 </Link>&nbsp;
-                            
+
+                                {/* Add Distribusi */}
                                 <Link to="AddStok">
-                                    <button className="px-4 py-2 btn btn-accent text-white">
-                                        Add Distribusi
-                                    </button>
+                                    <button className="px-4 py-2 bg-accent text-white rounded hover:bg-green-400 transition-colors">+ Distribusi</button>
                                 </Link>
                             </td>
                         </tr>
